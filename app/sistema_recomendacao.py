b7499de25d20a6805bc2e2b1fe4fcdb42432e30a
Duarte Junior <duarte.jr105@gmail.com>
2022-07-17 17:39:41 -0300

    update app scripts

--- app/sistema_recomendacao.py
+++ app/sistema_recomendacao.py
@@ -1,48 +1,68 @@
 import streamlit as st
-import pandas as pd
-from joblib import load
-import os
 from utils import load_data, select_data, recomendation
 
-st.title('WINER your personal sommelier.')
-data_load_state = st.text('Loading data...')
+# -- Loading the data
 data = load_data()
-data_load_state.text('Loading data...done!')
 
+# -- Sidebar --
+# Buttons to select the selection criteria
 sort_by = st.sidebar.radio("Sort wines by:",
                            ('points', 'price'))
+
+# Buttons how the data will be ordered whe is showed in the main page
 order = st.sidebar.radio('Order:',
                          ('descending', 'ascending'))
 
+# Box to select a list of styles to filter the data
 styles = st.sidebar.multiselect('Select some style of wine.',
-                               data['style'].unique())
+                                data['style'].unique())
+
+# Box to select a list of countries to filter the data
 countries = st.sidebar.multiselect('Select some contries.',
-                       data['country'].unique())
+                                   data['country'].unique())
+
+# Slider to select the points range to filter the data
 points = st.sidebar.slider('Interval of avaliation.', 80, 100, (80, 100))
+
+# Box to define the minimum wine value to filter the data
 min_price = st.sidebar.number_input('Minimum price:',
                                     min_value = data.price.min(),
-                                    max_value = data.price.max())
+                                    max_value = data.price.max(),
+                                    value = data.price.min())
+
+# Box to define the maximum vale to filter the data
 max_price = st.sidebar.number_input('Maximum price:',
                                     min_value = min_price,
                                     max_value = data.price.max(),
                                     value = data.price.max())
 
-price = [min_price, max_price]
 
+# -- Main page -- 
+st.title('WINER your personal sommelier.') # Main page title
+
+# Set styles in the case when nothing is selected in the sidebar
 if not styles:
     styles = data['style'].unique()
 
+# set countries in the when nothing is selected in the sidebar
 if not countries:
     countries = data['country'].unique()
     
-st.text(points)
+price_interval = [min_price, max_price]
 
+# Main table where are showed the top 5 wines considering the marked filters in the sidebar
+selected_data = select_data(data, styles, countries, points, price_interval, 
+                            sort_by, order) # data showed in the main table
+st.table(selected_data.head(5)) # Shows the table
+
+# -- Recomeded wines --
 st.subheader('We recomend these wines for you')
-
-selected_data = select_data(data, styles, countries, points, price, sort_by, order)
-
-st.table(selected_data.head(5))
-
 st.write('Chose a wine to see its description and more wine related with him.')
-wine_selected = st.selectbox('Chose a wine:', selected_data['title'].unique())
-st.table(recomendation(wine_selected, data)[['title', 'description', 'similarity (%)', 'style', 'variety', 'country', 'province', 'price', 'points']])
+
+# wine that will be used as the input for the recomendation system
+wine_selected = st.selectbox('Chose a wine:', selected_data['title'].head(5)) 
+
+# Table with the wines more related with the wine_selected
+st.table(recomendation(wine_selected, data)[['title', 'description', 'similarity (%)', 
+                                             'style', 'variety', 'country', 'province', 
+                                             'price', 'points']])
--- app/utils.py
+++ app/utils.py
@@ -1,23 +1,45 @@
 #%%
-import streamlit as st
+import os
 import numpy as np
 import pandas as pd
+import streamlit as st
 from joblib import load
-import os
 
-cwd = os.getcwd()
-clf = load(f'{cwd}/app/classifier.joblib')
-vetorizador = load(f'{cwd}/app/vectorizer.joblib')
+cwd = os.getcwd() # The current work directory
+classifier = load(f'{cwd}/app/classifier.joblib') # git
+#classifier = load(f'{cwd}/classifier.joblib') # local
+vectorizer = load(f'{cwd}/app/vectorizer.joblib') # git
+#vectorizer = load(f'{cwd}/vectorizer.joblib') # local
+
 system_data = pd.read_csv('https://raw.githubusercontent.com/duartejr/wine_system_recomendation/main/data/processed/wines_recomendation_system.csv')
 
 @st.cache
 def load_data():
+    """Load and returns the data
+
+    Returns:
+        pd.DataFrame: The data loaded
+    """
     data = pd.read_csv(f'https://raw.githubusercontent.com/duartejr/wine_system_recomendation/main/data/processed/wines_user_consult.csv')
     data = data.drop_duplicates()
     return data
 
 @st.cache
 def select_data(data, styles, countries, points, price, sort_by, order):
+    """Select data according with the filters passed.
+
+    Args:
+        data (pd.DataFrame): Dataframe with the wines data.
+        styles (list): List of strings with the wine styles to filter the data.
+        countries (list): List of strings with the countries names to filter the data.
+        points (list): List of floats containing a range of points to filter the data.
+        price (list): List of floats containing a range of prices to filter the data.
+        sort_by (string): String ("points" or "price") of the criterion to sort the data.
+        order (string): String ("ascending" or "descending") of the criterion to order the data.
+        
+    Returns:
+        data: Dataframe with the filtered data.
+    """
     data = data[data['style'].isin(styles)]
     data = data[data['country'].isin(countries)]
     data = data[data['points'].between(points[0], points[1])]
@@ -27,15 +49,26 @@ def select_data(data, styles, countries, points, price, sort_by, order):
         order = True
     else:
         order = False
+    
     data = data.sort_values(by=sort_by, ascending=order)
-    return data[['title', 'variety', 'style', 'winery', 'province', 'country', 'points', 'price']]
+    
+    return data[['title', 'variety', 'style', 'winery', 'province', 'country', 
+                 'points', 'price']]
 
-#%%
+@st.cache
 def recomendation(wine, user_data):
+    """Algorithm responsible to redomends wines that are similar with the one informed.
+
+    Args:
+        wine (string): Title of the wine which will be used as references to recomend another wines.
+        user_data (dataframe): A dataframe with the data which will be shown for the user.
+
+    Returns:
+        dataframe: A daframe with the wines more related with the informed wine considering the description.
+    """
     test = system_data.query(f"title == '{wine}'")
-    user_count_vec = vetorizador.transform(test.description)
-    NNs_clf = clf.kneighbors(user_count_vec, return_distance=True)
+    user_count_vec = vectorizer.transform(test.description)
+    NNs_clf = classifier.kneighbors(user_count_vec, return_distance=True)
     answer = user_data.loc[NNs_clf[1][0, :]]
     answer['similarity (%)'] = np.round(100 * (1 - NNs_clf[0][0, :]), 2)
     return answer
-#%%
